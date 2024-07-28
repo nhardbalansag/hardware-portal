1d69f9a13a69282c5467060768307a81bb55621c
nhardbalansag <nhardbalansag@gmail.com>
2024-07-29 01:13:31 +0800

    add images

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,25 +2,19 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-function Category({route = '/products', image = 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp'}) {
+function Category({route = '/products', sub='title here', title='Title', image = 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp'}) {
   return (
     <div 
-    style={{
-      backgroundImage: `url(${image})`,
-      backgroundSize:'cover',
-      backgroundRepeat:'no-repeat',
-      backgroundPosition:'center'
-    }}
-    className="m-5 shadow-xl card bg-base-100 image-full w-96">
-        {/* <figure>
-            <img
-                style={{ width: '100%', height: '100%' }}
-                src={image}
-                alt="Shoes" />
-        </figure> */}
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundSize:'cover',
+        backgroundRepeat:'no-repeat',
+        backgroundPosition:'center'
+      }}
+      className="m-5 shadow-xl card bg-base-100 image-full w-96">
         <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">{title}</h2>
+            <p>{sub}</p>
             <div className="justify-end card-actions">
               <Link to={route}>
                 <button className="btn btn-primary">See More</button>
--- src/pages/public/home/home.jsx
+++ src/pages/public/home/home.jsx
@@ -4,10 +4,12 @@ import Category from '../../../components/Category'
 
 import IconArrowDownUp from '../../../assets/icons/IconArrowDownUp'
 
-import test from '../../../../src/assets/img/fotis-nakos-lu0H9DVMBLE-unsplash.jpg'
+import test from '../../../../src/assets/img/christopher-burns-Wiu3w-99tNg-unsplash.jpg'
 import test2 from '../../../../src/assets/img/benjamin-lehman-EJU7A__krX0-unsplash.jpg'
 import test3 from '../../../../src/assets/img/sven-brandsma-kerFMg52cUA-unsplash.jpg'
 import test4 from '../../../../src/assets/img/greyson-joralemon-A1g0oeX29ec-unsplash.jpg'
+import test5 from '../../../../src/assets/img/jakub-zerdzicki-HXpG1S-INN8-unsplash.jpg'
+
 
 
 import HomeDisplayImage from '../../../../src/assets/img/julie-molliver-BYTMWQfQU4Y-unsplash.jpg'
@@ -25,7 +27,7 @@ function Home() {
         <div className="max-w-2xl py-32 sm:py-48 ">
           <div className="text-center md:mt-24" >
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Lorem ipsum dolor sit amet, consectetur .</h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">Empowering Your Build with Quality Hardware</p>
           </div>
         </div>
       </div>
@@ -33,14 +35,15 @@ function Home() {
         <IconArrowDownUp/>
       </div>
       <div class="mx-auto max-w-2xl text-center">
-        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Lorem ipsum </p>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Products and Services </p>
+        <p class="mt-6 text-lg leading-8 text-gray-600">Discover Our Exclusive Collections and Expert Services</p>
       </div>
       <div class="mx-auto max-w-2xl mt-3 lg:max-w-4xl justify-center flex flex-wrap">
-        <Category image={test}/>
-        <Category image={test3}/>
-        <Category image={test2}/>
-        <Category image={test4}/>
+        <Category sub='Top-quality welding products for strong, precise fabrication and repairs' title='Welding' image={test}/>
+        <Category sub='Premium painting supplies with a vibrant color palette for a flawless finish.' title='Painting' image={test3}/>
+        <Category sub='Durable and precise tools for tackling any project with ease.' title='Tools' image={test2}/>
+        <Category sub='Quality woodwork products for crafting elegant furniture and unique decor.' title='Wood Works' image={test4}/>
+        <Category sub='Reliable electrical products for safe and efficient installations and repairs.' title='Electrical' image={test5}/>
       </div>
     </div>
   )
